ea029de7003a1ea77e92daa82129dd0375a59afe
wafeelka <49671882+wafeelka@users.noreply.github.com>
2020-07-11 15:24:56 +0300

    add a main files
    GameManager script needs for instantiating targets randomly and updating player's score.
    Targets script is describing the behavior of targets.

---
+++ GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> gameObjects;
+    public ParticleSystem explosion;
+    private const float spanTime = 2.5f;
+    
+    public TextMeshProUGUI scoreText;
+     int score;
+
+    void Start()
+    {
+        score = 0;
+        StartCoroutine(spawnTarget());
+        UpdateScore(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator spawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spanTime);
+            int index = Random.Range(0, gameObjects.Count);
+            Instantiate(gameObjects[index]);
+        }
+       
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
+    }
+}
---
+++ Targets.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Targets : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float xPos = 7f;
+    public int point;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        transform.position = new Vector3(GetRandomValue(xPos), -2); // start spawn position all targets 
+        targetRB = GetComponent<Rigidbody>();
+        targetRB.AddForce(Vector3.up * GenerateSpeed(), ForceMode.Impulse);
+        targetRB.AddTorque(GetRandomValue(10), GetRandomValue(15), GetRandomValue(12), ForceMode.Impulse);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(point);
+        Instantiate(gameManager.explosion, transform.position, gameManager.explosion.transform.rotation);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    private float GenerateSpeed()
+    {
+        return Random.Range(13.0f, 16.0f);
+    }
+    private float GetRandomValue(float value)
+    {
+        return Random.Range(-value, value);
+    }
+    
+}
+
